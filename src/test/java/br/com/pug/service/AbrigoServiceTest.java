a0cdce5054e38d821d19c558329a57aef3589f64
Lucas Cunha <lucas71cunha@gmail.com>
2024-10-06 00:34:53 -0300

    Test Refactoring

--- src/test/java/br/com/pug/service/AbrigoServiceTest.java
+++ src/test/java/br/com/pug/service/AbrigoServiceTest.java
@@ -19,10 +19,10 @@ public class AbrigoServiceTest {
     private ClientHttpConfiguration client = mock(ClientHttpConfiguration.class);
     private AbrigoService abrigoService = new AbrigoService(client);
     private HttpResponse<String> response = mock(HttpResponse.class);
-    private Abrigo abrigo = new Abrigo("Teste", "61981880392", "abrigo_pug@gmail.com");
+    private Abrigo abrigo = new Abrigo("Teste", "1199999999", "abrigo_pug@gmail.com");
 
     @Test
-    public void deveVerificarSeDispararRequisicaoGetSeraChamado() throws IOException, InterruptedException {
+    public void deveVerificarQuandoHaAbrigo() throws IOException, InterruptedException {
         abrigo.setId(0L);
         String expectedAbrigosCadastrados = "Abrigos cadastrados:";
         String expectedIdENome = "0 - Teste";
@@ -42,6 +42,28 @@ public class AbrigoServiceTest {
 
         Assertions.assertEquals(expectedAbrigosCadastrados, actualAbrigosCadastrados);
         Assertions.assertEquals(expectedIdENome, actualIdENome);
+    }
+
+    @Test
+    public void deveVerificarQuandoNaoHaAbrigo() throws IOException, InterruptedException {
+        abrigo.setId(0L);
+        String expected = "Não há abrigos cadastrados";
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(baos);
+        System.setOut(printStream);
+
+        when(response.body()).thenReturn("[]");
+        when(client.dispararRequisicaoGet(anyString())).thenReturn(response);
+
+        abrigoService.listarAbrigo();
+
+        String[] lines = baos.toString().split(System.lineSeparator());
+        String actual = lines[0];
+
+
+        Assertions.assertEquals(expected, actual);
+
     }
 
 }
